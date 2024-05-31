af66494f15314bcb6827c04b7214945f73798fd6
sanderik229 <144127176+sanderik229@users.noreply.github.com>
2024-05-31 20:31:56 +0300

    1232134

---
+++ LibraryDeSerealize/LibraryDeSerealize/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Windows;
+using SerializationLibrary;
+
+namespace WPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+
+            Person person = new Person { Name = "Максим Лавров", Age = 30 };
+            byte[] serializedData = new Serializer().Serialize(person);
+            Person deserializedPerson = new Serializer().Deserialize<Person>(serializedData);
+
+            Console.WriteLine("Сериализация: " + serializedData.Length);
+            Console.WriteLine("Десериализация: " + deserializedPerson.Name + " " + deserializedPerson.Age);
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ LibraryDeSerealize/SerializationLibrary/Serializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializationLibrary
+{
+    public class Serializer
+    {
+        public byte[] Serialize<T>(T obj)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        public T Deserialize<T>(byte[] data)
+        {
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
